d37359f055da1aa0e7657c710ecb6d1fc3556176
farhannjb <farhan@koda.so>
2025-10-23 12:33:34 +0700

    validator with authGuards

---
+++ src/common/resources/auth/dto/login.dto.ts
@@ -0,0 +1,13 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, MinLength } from 'class-validator';
+
+@InputType()
+export class LoginDto {
+  @Field({ nullable: true })
+  @IsEmail()
+  email: string;
+
+  @Field({ nullable: true })
+  @MinLength(6)
+  password: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { JwtAuthExceptionFilter } from './common/resources/auth/filters/jwt-auth.exception.filter';
 import { GraphQLLoggingInterceptor } from './application/logging';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // dev
   app.useGlobalInterceptors(new GraphQLLoggingInterceptor());
+  app.useGlobalFilters(new JwtAuthExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
